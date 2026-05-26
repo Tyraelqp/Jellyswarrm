0b6003c506c948969b1697529554e21be97d8d94
Sergii Fokin <tyraelqp@gmail.com>
2026-05-26 17:06:27 +0300

    Added playlists support

--- crates/jellyswarrm-proxy/src/main.rs
+++ crates/jellyswarrm-proxy/src/main.rs
@@ -514,6 +514,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         post(handlers::items::post_playback_info),
                     ),
             )
+            .route("/Playlists/{item_id}/Items", get(handlers::items::get_items))
             .route("/MediaSegments/{item_id}", get(handlers::items::get_items))
             // Show-specific routes
             .nest(
